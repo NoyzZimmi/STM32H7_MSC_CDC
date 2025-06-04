502670c26adb29e0da17fb1454007c9d156b3bae
Daniel Zimmermann <dazi@nktphotonics.com>
2025-06-04 16:26:35 +0200

    Recreated SOM board setup on Nucleo dev kit

---
+++ Core/Src/main.c
@@ -0,0 +1,383 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "app_threadx.h"
+#include "main.h"
+#include "eth.h"
+#include "memorymap.h"
+#include "usb_otg.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "sdmmc.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+COM_InitTypeDef BspCOMInit;
+__IO uint32_t BspButtonState = BUTTON_RELEASED;
+
+/* USER CODE BEGIN PV */
+HAL_SD_CardInfoTypeDef USBD_SD_CardInfo;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MPU_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MPU Configuration--------------------------------------------------------*/
+  MPU_Config();
+
+  /* Enable the CPU Cache */
+
+  /* Enable I-Cache---------------------------------------------------------*/
+  SCB_EnableICache();
+
+  /* Enable D-Cache---------------------------------------------------------*/
+  SCB_EnableDCache();
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  MX_ETH_Init();
+  /* USER CODE BEGIN 2 */
+
+  if (GPIO_SD_isCardPresent())
+  {
+      // Init SD card
+      MX_SDMMC1_SD_Init();
+
+      // Check card status
+      uint32_t sdCardstart = HAL_GetTick();
+      while (HAL_GetTick() - sdCardstart < 2000) {
+          if(HAL_SD_GetCardState(&hsd1) == HAL_SD_CARD_TRANSFER) {
+              break;
+          }
+      }
+
+      // Get card info
+      if (HAL_SD_GetCardInfo(&hsd1, &USBD_SD_CardInfo) != HAL_OK)
+      {
+          Error_Handler();
+      }
+  }
+
+  /* USER CODE END 2 */
+
+  MX_ThreadX_Init();
+
+  /* Initialize leds */
+  BSP_LED_Init(LED_GREEN);
+  BSP_LED_Init(LED_BLUE);
+  BSP_LED_Init(LED_RED);
+
+  /* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
+  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
+
+  /* Initialize COM1 port (115200, 8 bits (7-bit data + 1 stop bit), no parity */
+  BspCOMInit.BaudRate   = 115200;
+  BspCOMInit.WordLength = COM_WORDLENGTH_8B;
+  BspCOMInit.StopBits   = COM_STOPBITS_1;
+  BspCOMInit.Parity     = COM_PARITY_NONE;
+  BspCOMInit.HwFlowCtl  = COM_HWCONTROL_NONE;
+  if (BSP_COM_Init(COM1, &BspCOMInit) != BSP_ERROR_NONE)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN BSP */
+
+  /* -- Sample board code to send message over COM1 port ---- */
+  printf("Welcome to STM32 world !\n\r");
+
+  /* -- Sample board code to switch on leds ---- */
+  BSP_LED_On(LED_GREEN);
+  BSP_LED_On(LED_BLUE);
+  BSP_LED_On(LED_RED);
+
+  /* USER CODE END BSP */
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+    /* -- Sample board code for User push-button in interrupt mode ---- */
+    if (BspButtonState == BUTTON_PRESSED)
+    {
+      /* Update button state */
+      BspButtonState = BUTTON_RELEASED;
+      /* -- Sample board code to toggle leds ---- */
+      BSP_LED_Toggle(LED_GREEN);
+      BSP_LED_Toggle(LED_BLUE);
+      BSP_LED_Toggle(LED_RED);
+
+      /* ..... Perform your action ..... */
+    }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_CRSInitTypeDef RCC_CRSInitStruct = {0};
+
+  /** Supply configuration update enable
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 60;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 20;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Enable the SYSCFG APB clock
+  */
+  __HAL_RCC_CRS_CLK_ENABLE();
+
+  /** Configures CRS
+  */
+  RCC_CRSInitStruct.Prescaler = RCC_CRS_SYNC_DIV1;
+  RCC_CRSInitStruct.Source = RCC_CRS_SYNC_SOURCE_USB2;
+  RCC_CRSInitStruct.Polarity = RCC_CRS_SYNC_POLARITY_RISING;
+  RCC_CRSInitStruct.ReloadValue = __HAL_RCC_CRS_RELOADVALUE_CALCULATE(48000000,1000);
+  RCC_CRSInitStruct.ErrorLimitValue = 34;
+  RCC_CRSInitStruct.HSI48CalibrationValue = 32;
+
+  HAL_RCCEx_CRSConfig(&RCC_CRSInitStruct);
+}
+
+/* USER CODE BEGIN 4 */
+
+bool GPIO_SD_isCardPresent(void) {
+	return HAL_GPIO_ReadPin(SD_DET_GPIO_Port, SD_DET_Pin) == GPIO_PIN_SET;
+}
+
+/* USER CODE END 4 */
+
+ /* MPU Configuration */
+
+void MPU_Config(void)
+{
+  MPU_Region_InitTypeDef MPU_InitStruct = {0};
+
+  /* Disables the MPU */
+  HAL_MPU_Disable();
+
+  /** Initializes and configures the Region and the memory to be protected
+  */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.BaseAddress = 0x0;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+  MPU_InitStruct.SubRegionDisable = 0x87;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  /** Initializes and configures the Region and the memory to be protected
+  */
+  MPU_InitStruct.Number = MPU_REGION_NUMBER1;
+  MPU_InitStruct.BaseAddress = 0x24078000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_32KB;
+  MPU_InitStruct.SubRegionDisable = 0x0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+  /* Enables the MPU */
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief BSP Push Button callback
+  * @param Button Specifies the pressed button
+  * @retval None
+  */
+void BSP_PB_Callback(Button_TypeDef Button)
+{
+  if (Button == BUTTON_USER)
+  {
+    BspButtonState = BUTTON_PRESSED;
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ USBX/App/ux_device_cdc_acm.c
@@ -0,0 +1,286 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    ux_device_cdc_acm.c
+  * @author  MCD Application Team
+  * @brief   USBX Device CDC ACM applicative source file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2020-2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "ux_device_cdc_acm.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "main.h"
+#include "app_threadx.h"
+
+//=============================================================================
+// Local macros, variables and constants
+//=============================================================================
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef struct {
+    uint8_t *data;
+    uint16_t length;
+} UsbTxPacket;
+
+typedef enum {
+    USB_CDC_EVENT_Connected = 1 << 0,
+} USB_CDC_EVENT_t;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define USB_BUFFER_SIZE 512
+
+#define USB_TX_BYTE_POOL_SIZE (1024 * 1) // 1 KB pool size
+#define USB_TX_QUEUE_SIZE 10
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+UX_SLAVE_CLASS_CDC_ACM  *cdc_acm;
+
+static uint8_t usb_rx_buffer[USB_BUFFER_SIZE];
+
+// USB TX queues and byte pools
+TX_BYTE_POOL usb_tx_byte_pool;
+uint8_t usb_tx_byte_pool_memory[USB_TX_BYTE_POOL_SIZE];
+
+TX_QUEUE usb_tx_queue;
+UsbTxPacket* usb_tx_queue_buffer[USB_TX_QUEUE_SIZE];
+
+TX_EVENT_FLAGS_GROUP usb_cdc_events;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+//=============================================================================
+// Local function prototypes
+//=============================================================================
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+//=============================================================================
+// Implementation
+//=============================================================================
+
+UINT USB_CDC_init() {
+    UINT ret;
+
+    // Create byte pool for outgoing USB messages
+    ret = tx_byte_pool_create(&usb_tx_byte_pool, "USB TX byte Pool",
+        usb_tx_byte_pool_memory, USB_TX_BYTE_POOL_SIZE);
+
+    if(ret != TX_SUCCESS) {
+        printf("Failed to create usb tx byte pool, error=%d\n", ret);
+        return ret;
+    }
+
+    // Create queue for outgoing USB messages
+    ret = tx_queue_create(&usb_tx_queue, "USB TX Queue",
+        sizeof(UsbTxPacket*) / sizeof(ULONG), usb_tx_queue_buffer,
+        sizeof(usb_tx_queue_buffer));
+
+    if(ret != TX_SUCCESS) {
+        printf("Failed to create usb tx queue, error=%d\n", ret);
+        return ret;
+    }
+
+    // create semaphore for connection handling
+    ret = tx_event_flags_create(&usb_cdc_events, "USB CDC events");
+    if(ret != TX_SUCCESS) {
+        printf("Failed to create usb cdc events, error=%d\n", ret);
+        return ret;
+    }
+
+    return ret;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  USBD_CDC_ACM_Activate
+  *         This function is called when insertion of a CDC ACM device.
+  * @param  cdc_acm_instance: Pointer to the cdc acm class instance.
+  * @retval none
+  */
+VOID USBD_CDC_ACM_Activate(VOID *cdc_acm_instance)
+{
+  /* USER CODE BEGIN USBD_CDC_ACM_Activate */
+  UX_PARAMETER_NOT_USED(cdc_acm_instance);
+  /* USER CODE END USBD_CDC_ACM_Activate */
+
+  return;
+}
+
+/**
+  * @brief  USBD_CDC_ACM_Deactivate
+  *         This function is called when extraction of a CDC ACM device.
+  * @param  cdc_acm_instance: Pointer to the cdc acm class instance.
+  * @retval none
+  */
+VOID USBD_CDC_ACM_Deactivate(VOID *cdc_acm_instance)
+{
+  /* USER CODE BEGIN USBD_CDC_ACM_Deactivate */
+  UX_PARAMETER_NOT_USED(cdc_acm_instance);
+  /* USER CODE END USBD_CDC_ACM_Deactivate */
+
+  return;
+}
+
+/**
+  * @brief  USBD_CDC_ACM_ParameterChange
+  *         This function is invoked to manage the CDC ACM class requests.
+  * @param  cdc_acm_instance: Pointer to the cdc acm class instance.
+  * @retval none
+  */
+VOID USBD_CDC_ACM_ParameterChange(VOID *cdc_acm_instance)
+{
+  /* USER CODE BEGIN USBD_CDC_ACM_ParameterChange */
+  UX_PARAMETER_NOT_USED(cdc_acm_instance);
+  /* USER CODE END USBD_CDC_ACM_ParameterChange */
+
+  return;
+}
+
+/* USER CODE BEGIN 1 */
+
+VOID usbx_cdc_read_thread_entry(ULONG thread_input)
+{
+    /* Local Variables */
+    ULONG actual_length;
+    ULONG eventFlags;
+    UX_SLAVE_DEVICE *device;
+    device = &_ux_system_slave->ux_system_slave_device;
+    UINT status = UX_SUCCESS;
+
+    /* Infinite Loop */
+    while(1)
+    {
+        // Wait for connection
+        tx_event_flags_get(&usb_cdc_events, USB_CDC_EVENT_Connected, TX_OR, &eventFlags, TX_WAIT_FOREVER);
+
+        while ((device->ux_slave_device_state == UX_DEVICE_CONFIGURED) && (cdc_acm != UX_NULL))
+        {
+            // read bytes from USB
+            status = ux_device_class_cdc_acm_read(cdc_acm, (UCHAR *)usb_rx_buffer, USB_BUFFER_SIZE / 2, &actual_length);
+
+            if(status == UX_SUCCESS && actual_length > 0) {
+
+                // TODO Put data on queue for echo
+            	// Usually internal processing here - now just put data on queue for echo
+
+            	UsbTxPacket *packet;
+            	uint8_t *packetData;
+
+            	// Allocate space for the packet structure
+            	if (tx_byte_allocate(&usb_tx_byte_pool, (VOID **)&packet, sizeof(UsbTxPacket), TX_NO_WAIT) != TX_SUCCESS) {
+            		printf("Failed to allocate packet structure\n");
+            		continue;
+            	}
+
+            	// Allocate space for the data buffer
+            	if (tx_byte_allocate(&usb_tx_byte_pool, (VOID **)&packetData, actual_length, TX_NO_WAIT) != TX_SUCCESS) {
+            		printf("Failed to allocate packetData\n");
+
+            		// release previous allocation
+            		tx_byte_release(packet);
+            		continue;
+            	}
+
+            	// copy incoming message to new packet
+            	memcpy(packetData, usb_rx_buffer, actual_length);
+            	packet->data = packetData;
+            	packet->length = actual_length;
+
+            	// Add to queue (if full, handle overflow case)
+            	if (tx_queue_send(&usb_tx_queue, &packet, TX_NO_WAIT) != TX_SUCCESS) {
+            		printf("TX queue full, dropping packet\n");
+
+            		// release previous allocation
+            		tx_byte_release(packetData);
+            		tx_byte_release(packet);
+            		continue;
+            	}
+            }
+            else if (status == UX_ERROR || device->ux_slave_device_state != UX_DEVICE_CONFIGURED)
+            {
+                // Disconnected or timeout, break and wait for reconnect
+                break;
+            }
+        }
+
+        // Optional short sleep to reduce CPU usage in case of polling disconnect state
+        tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND / 10);
+    }
+}
+
+VOID usbx_cdc_write_thread_entry(ULONG thread_input)
+{
+    /* Local Variables */
+    UX_SLAVE_DEVICE *device;
+    device = &_ux_system_slave->ux_system_slave_device;
+
+    ULONG eventFlags;
+    UsbTxPacket *packet;
+    ULONG bytesWritten;
+    UINT status;
+
+    /* Infinite Loop */
+    while (1)
+    {
+        // Wait for connection
+        tx_event_flags_get(&usb_cdc_events, USB_CDC_EVENT_Connected, TX_OR, &eventFlags, TX_WAIT_FOREVER);
+
+        // block until a message is ready to be sent
+        if (tx_queue_receive(&usb_tx_queue, &packet, TX_WAIT_FOREVER) == TX_SUCCESS)
+        {
+            // packet was successfully fetched from queue
+            // Check if device is still properly configured
+            if ((device->ux_slave_device_state == UX_DEVICE_CONFIGURED) && (cdc_acm != UX_NULL))
+            {
+                // Send over USB
+                status = ux_device_class_cdc_acm_write(cdc_acm, packet->data, packet->length, &bytesWritten);
+                if (status != UX_SUCCESS)
+                {
+                    printf("Write failed with status: %d\n", status);
+                }
+            }
+
+            // Clean up transfer - release memory after transmission
+            tx_byte_release(packet->data);
+            tx_byte_release(packet);
+        }
+    }
+}
+
+/* USER CODE END 1 */
